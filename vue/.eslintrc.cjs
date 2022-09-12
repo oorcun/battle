8535003af2a1fdb5aad7786d02a9e1cf2245fae9
oorcun <orcunaltinsoy@gmail.com>
2022-09-12 17:55:11 +0300

    Configured eslint.

--- vue/.eslintrc.cjs
+++ vue/.eslintrc.cjs
@@ -1,21 +1,39 @@
 /* eslint-env node */
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+	root: true,
+	'extends': [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended'
+	],
+	overrides: [
+		{
+			files: [
+				'cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}'
+			],
+			'extends': [
+				'plugin:cypress/recommended'
+			]
+		}
+	],
+	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	},
 }
