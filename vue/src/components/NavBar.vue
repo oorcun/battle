8c7eb176cdfc9fab19b32fb14303da51e6f7b8fb
oorcun <orcunaltinsoy@gmail.com>
2022-09-12 20:36:53 +0300

    Added navbar.

--- vue/src/components/NavBar.vue
+++ vue/src/components/NavBar.vue
@@ -7,7 +7,28 @@
 
 <template>
 
-	<h1>navbar</h1>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+	<div id="navbarBasicExample" class="navbar-menu">
+		<div class="navbar-start">
+			<a class="navbar-item">
+				Players
+			</a>
+			<a class="navbar-item">
+				About
+			</a>
+		</div>
+    </div>
+
+    <div class="navbar-end">
+		<div class="navbar-item">
+			<div class="buttons">
+				<a class="button is-primary">
+					<strong>Connect MetaMask</strong>
+				</a>
+			</div>
+		</div>
+	</div>
+</nav>
 
 </template>
 
