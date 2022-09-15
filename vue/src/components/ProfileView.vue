4db18820e2bf7805135c405419b1c89fb3ad29f6
oorcun <orcunaltinsoy@gmail.com>
2022-09-15 23:00:02 +0300

    Added environment variables.

--- vue/src/components/ProfileView.vue
+++ vue/src/components/ProfileView.vue
@@ -8,6 +8,13 @@ export default {
 		MetamaskNotification
 	}
 
+	// const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+	// new web3.eth.Contract(jsonInterface[, address][, options])
+	// myContract.methods.myMethod(123).call({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'})
+	// .then(function(result){
+	//     ...
+	// });
+
 	// fetch player
 	// player form if player not exist
 	// player store
