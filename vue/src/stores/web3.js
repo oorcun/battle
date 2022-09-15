55ee138e125368601f869825d870fd8ca56d9af9
oorcun <orcunaltinsoy@gmail.com>
2022-09-16 02:39:30 +0300

    Added get player method.

--- vue/src/stores/web3.js
+++ vue/src/stores/web3.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import config from '../../web3.config.js'
 import playerContractAbi from '../components/abis/PlayerContract.js'
+import { useMetamaskStore } from './metamask.js'
 
 export const useWeb3Store = defineStore('web3', {
 	state: () => ({
@@ -11,11 +12,18 @@ export const useWeb3Store = defineStore('web3', {
 		assignStates () {
 			const network = config.networks[config.currentNetwork]
 			this.web3 = new Web3(network.provider)
-
 			this.playerContract = new this.web3.eth.Contract(
 				playerContractAbi,
 				config.addresses.playerContract
 			)
+		},
+		getPlayer () {
+			const metamaskStore = useMetamaskStore()
+			this.playerContract.methods.getPlayer().call({ from: metamaskStore.account })
+				.then(result => {
+					console.log(result)
+				})
+				.catch(console.error)
 		}
 	}
 })
