18ea2b7f393da0d988cd49dd6bbe66879e362ff4
oorcun <orcunaltinsoy@gmail.com>
2022-09-17 02:15:01 +0300

    Added table for player info.

--- vue/src/components/ProfileView.vue
+++ vue/src/components/ProfileView.vue
@@ -42,10 +42,24 @@ export default {
 <hr>
 
 <div v-if="metamaskState === 'connected'" class="box">
+
 	<template v-if="playerState === 'exist'">
-		<div @click="player = []">{{ player }}</div>
+		<table class="table">
+			<tbody>
+				<tr><th>ID</th><td>{{ player[0] }}</td></tr>
+				<tr><th>Name</th><td>{{ player[1] }}</td></tr>
+				<tr><th>Attacking Wins</th><td>{{ player[2] }}</td></tr>
+				<tr><th>Attacking Losses</th><td>{{ player[3] }}</td></tr>
+				<tr><th>Defending Wins</th><td>{{ player[4] }}</td></tr>
+				<tr><th>Defending Losses</th><td>{{ player[5] }}</td></tr>
+				<tr><th>Total Wins</th><td>{{ parseInt(player[2]) + parseInt(player[4]) }}</td></tr>
+				<tr><th>Total Losses</th><td>{{ parseInt(player[3]) + parseInt(player[5]) }}</td></tr>
+				<tr><th>Points</th><td>{{ player[6] }}</td></tr>
+			</tbody>
+		</table>
 		<div>getpastevents</div>
 	</template>
+
 	<template v-else-if="playerState === 'notExist'">
 		<div class="notification is-info is-light">
 			Please create a player.
@@ -62,11 +76,13 @@ export default {
 			</div>
 		</div>
 	</template>
+
 	<template v-else>
 		<div class="notification is-info is-light">
 			Please wait while your player is fetching from the network. <img src="src/components/gifs/loading-loading-forever.gif">
 		</div>
 	</template>
+
 </div>
 
 </template>
