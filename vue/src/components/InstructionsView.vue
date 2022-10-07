4404584c6c605c36fbee16f96cfa98480cfc2478
oorcun <orcunaltinsoy@gmail.com>
2022-10-08 01:46:30 +0300

    Added instructions.

--- vue/src/components/InstructionsView.vue
+++ vue/src/components/InstructionsView.vue
@@ -7,7 +7,20 @@
 
 <template>
 
-<h1>InstructionsView</h1>
+<ol>
+	<li>Install <a href="https://metamask.io" target="_blank">Metamask <ion-icon name="arrow-redo"></ion-icon></a></li>
+	<li>Select Rinkeby network for testing</li>
+	<li>Connect wallet</li>
+	<li>Go to <i>Players</i> and click <i>Attack</i></li>
+	<li>Select <i>Starting Minute</i> for the attack</li>
+	<li>Choose <i>Prediction</i> for the Bitcoin price for your selected minute</li>
+	<li>Click <i>Register Attack</i> and wait</li>
+	<li>If you predict correctly you win otherwise you lose</li>
+</ol>
+
+<footer class="footer">
+	More information on <a href="https://github.com/oorcun/battle" target="_blank">GitHub <ion-icon name="arrow-redo"></ion-icon></a>
+</footer>
 
 </template>
 
