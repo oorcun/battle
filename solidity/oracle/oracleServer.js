c913fb67a4eacd49086d95459348d1a504f03ccc
oorcun <orcunaltinsoy@gmail.com>
2022-10-28 18:58:29 +0300

    Assembled two files.

--- solidity/oracle/oracleServer.js
+++ solidity/oracle/oracleServer.js
@@ -1,5 +1,47 @@
 const http = require('http')
-const { isOracleRunning, startOracle, stopOracle } = require('./oracleOperations.js')
+const fs = require('fs')
+const childProcess = require('child_process')
+const pidFile = __dirname + '/pid'
+
+function isOracleRunning () {
+	try {
+		const processId = Number(fs.readFileSync(pidFile))
+		process.kill(processId, 0)
+		return true
+	} catch {
+		return false
+	}
+}
+
+function stopOracle () {
+	try {
+		const processId = Number(fs.readFileSync(pidFile))
+		process.kill(processId, 'SIGINT')
+		return true
+	} catch {
+		return false
+	}
+}
+
+function startOracle () {
+	try {
+		if (isOracleRunning()) {
+			return false
+		}
+		const subProcess = childProcess.spawn(
+			'truffle',
+			['exec', 'oracle/oracled.js', '--network', 'goerli', '>', 'log/oracle.log'],
+			{
+				stdio: 'ignore',
+				shell: true
+			}
+		)
+		subProcess.unref()
+		return true
+	} catch {
+		return false
+	}
+}
 
 const server = http.createServer()
 	.on('request', (request, response) => {
