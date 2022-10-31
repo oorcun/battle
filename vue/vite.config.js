3278f53274dcb0c15ad1c0f6cf3c9d9ee23ffbf0
oorcun <orcunaltinsoy@gmail.com>
2022-11-01 02:24:41 +0300

    Fixed Vue router in production.

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '',
+	base: '/battle/',
 	plugins: [vue({
 		template: {
 			compilerOptions: {
